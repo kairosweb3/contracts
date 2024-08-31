60ea17dfbd2782bbecf1e01eaae82b4ec1f185b7
0xp3gasus <0xp3gasus@proton.me>
2024-09-01 00:34:32 +0200

    Swap test skeleton

--- test/Migration.test.ts
+++ test/Migration.test.ts
@@ -1,13 +1,22 @@
 import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { expect } from "chai";
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
 import { main as mintNewPosition } from "../scripts/mintNFTPosition";
 import { main as listPositions } from "../scripts/listPositions";
+import { main as getAssetFromEth } from "../scripts/getAssetFromEth";
 
 const INonfungiblePositionManager = require('@uniswap/v3-periphery/artifacts/contracts/NonfungiblePositionManager.sol/NonfungiblePositionManager.json').abi;
+
+const ERC20_ABI = [
+  "function balanceOf(address owner) view returns (uint256)",
+  "function transfer(address to, uint amount) returns (bool)",
+]
+
 const UNI_V3_POS = '0x03a520b32C04BF3bEEf7BEb72E919cf822Ed34f1' 
+const UNI_UNIVERSAL_ROUTER = "0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD";
 const WETH_ADDRESS = "0x4200000000000000000000000000000000000006";
 const WEWE_ADDRESS = "0x6b9bb36519538e0C073894E964E90172E1c0B41F";
+const USDC_ADDRESS = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
 
 
 describe("Migration contract", function () {
@@ -15,7 +24,13 @@ describe("Migration contract", function () {
     const [owner, otherAccount] = await ethers.getSigners();
     const Migration = await ethers.getContractFactory("Migration");
 
-    const migration = await Migration.deploy(UNI_V3_POS, WEWE_ADDRESS, WETH_ADDRESS);
+    const migration = await Migration.deploy(
+      UNI_V3_POS,
+      WEWE_ADDRESS,
+      WETH_ADDRESS,
+      UNI_UNIVERSAL_ROUTER,
+      USDC_ADDRESS
+    );
 
     return { migration, owner, otherAccount };
   }
@@ -47,4 +62,27 @@ describe("Migration contract", function () {
       expect(contractBalanceAfter).is.greaterThan(contractBalanceBefore); 
     });
   });
+
+  describe('Swap', () => {
+    it('Should do swap', async () => {
+      const { migration, owner } = await loadFixture(deployFixture)
+      const wethContract = new ethers.Contract(WETH_ADDRESS, ERC20_ABI, owner)
+      const usdcContract = new ethers.Contract(USDC_ADDRESS, ERC20_ABI, owner)
+
+      await getAssetFromEth(await owner.getAddress())
+
+      const preWETHBalance = await wethContract.balanceOf(await owner.getAddress())
+      const preUSDCBalance = await usdcContract.balanceOf(await owner.getAddress())
+      
+      // const swapTx = await migration.swap(await wethContract.getAddress(), ethers.parseEther('1'))
+      // await swapTx.wait()
+
+      const postWETHBalance = await wethContract.balanceOf(await owner.getAddress())
+      const postUSDCBalance = await usdcContract.balanceOf(await owner.getAddress())
+
+      expect(preWETHBalance).is.greaterThan(postWETHBalance)
+      expect(preUSDCBalance).is.lessThan(postUSDCBalance)
+
+    })
+  })
 });
